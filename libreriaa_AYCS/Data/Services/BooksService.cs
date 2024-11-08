aca2e74d348f9d2a6247a769683b6b1aa8b66f42
LDVP5049 <luismaster752@gmail.com>
2024-11-08 13:25:17 -0600

    terminado

--- libreriaa_AYCS/Data/Services/BooksService.cs
+++ libreriaa_AYCS/Data/Services/BooksService.cs
@@ -1,6 +1,9 @@
 ﻿using libreriaa_AYCS.Data.ViewModels;
 using libreriaa_AYCS.Data.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 namespace libreriaa_AYCS.Data.Services
 {
     public class BooksService
@@ -27,6 +30,39 @@ namespace libreriaa_AYCS.Data.Services
             };
             _context.Books.Add(_book);
             _context.SaveChanges();
+        }
+        //metodo que nos permite obtener una lista de todos los libros de la BD
+         public List<Book> GetAllBks() => _context.Books.ToList();
+        //metodo que nos permite obtener el libro por ID de la BD
+       public Book GetBookById(int bookid) => _context.Books.FirstOrDefault(n => n.id == bookid);
+        //metodo que nos permite editar una libro de todos los libros de la BD
+        public Book UpdateBookById(int bookid, BookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.id == bookid); 
+            if (_book == null)
+            {
+                _book.Titulo = book.Titulo;
+                _book.Descripcion = book.Descripcion;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.DateRead;
+                _book.Rate = book.Rate;
+                _book.Genero = book.Genero;
+                _book.Autor = book.Autor;
+                _book.CoverUrl = book.CoverUrl;
+
+                _context.SaveChanges();
+            }
+            return _book;
+        }
+
+        public void DeleteBookyById(int bookid)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.id == bookid);
+            if ( _book != null)
+            {
+                _context.Books.Remove( _book );
+                _context.SaveChanges();
+            }
         }
 
     }
